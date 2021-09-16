43bc2e275e1d59d018269211aafdc3774a2fbfa5
Jim Rogers <thejimrogers@pm.me>
2021-09-16 11:29:07 -0500

    finished reverse app
    may need minor revisions in near future

--- js/site.js
+++ js/site.js
@@ -1,4 +1,32 @@
-// Call Hello World
-function helloWorld() {
-  alert("Hello, World!");
+//Get string from app UI
+//Controller function
+function getValue() {
+  document.getElementById("alert").classList.add("invisible");
+  let userString = document.getElementById("userString").value;
+  let revString = reverseString(userString);
+  displayString(revString);
+}
+
+//Reverse the string
+//Logic function
+function reverseString(userString) {
+  let revString = [];
+
+  //Reverse string using for loop
+  for (let index = userString.length - 1; index >= 0; index--) {
+    revString += userString[index];
+  }
+  return revString;
+}
+
+//Display  reversed string to user
+//View function
+function displayString(revString) {
+  //Write message to page
+  document.getElementById(
+    "msg"
+  ).innerHTML = `Your string reversed is: ${revString}`;
+
+  //Show alert box
+  document.getElementById("alert").classList.remove("invisible");
 }
